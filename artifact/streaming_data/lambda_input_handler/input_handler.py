a4ebe4df254225e2ddd766b3f819147a69889b60
Kim Eunho <thinkingim@gmail.com>
2021-09-28 08:37:57 +0000

    lambda msk producer

--- artifact/streaming_data/lambda_input_handler/input_handler.py
+++ artifact/streaming_data/lambda_input_handler/input_handler.py
@@ -1,5 +1,7 @@
 import os
 import boto3
+from kafka import KafkaProducer
+from json import dumps
 
 MSK_CLUSTER = os.environ['msk_cluster_arn']
 
@@ -8,5 +10,16 @@ RESPONSE = CLIENT.get_bootstrap_brokers(
     ClusterArn=MSK_CLUSTER
 )
 
+PRODUCER = KafkaProducer(
+    acks=0, 
+    compression_type='gzip', 
+    bootstrap_servers=['localhost:9092'], 
+    value_serializer=lambda x: dumps(x).encode('utf-8'))
+
 def do(event: dict, context):
     print(RESPONSE)
+
+    data = {'str' : 'result-test'}
+    PRODUCER.send('test', value=data)
+    PRODUCER.flush()
+    
